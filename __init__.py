0a115f3e7a8ad923ac8e07addca5b91b1d568bb0
劉達 <ryu@anchorz.co.jp>
2021-09-26 19:39:56 +0900

    初めのロジック入れ

--- __init__.py
+++ __init__.py
@@ -1,81 +1,26 @@
-import random
+from typing import List, Callable
 
-from fastapi import APIRouter, Body, Depends
-
-from app.core.database_engine.DbAdaptor import DbAdaptor
-from app.core.module_class import TableModule, ApiModule
-from app.core.page_engine.PageAdaptor import PageAdaptor
-from app.modules.sample.table import SampleTable
+from app.core.module_class import Module, TableModule
+from app.core.module_class.SecurityModule import SecurityModule
+from app.modules.simple_key.table import SimpleKeyTable
 
 
-# TODO: change class name
-class Sample(ApiModule, TableModule):
-    def _register_api_bp(self, bp: APIRouter):
-        @bp.get('/description')
-        def description():
-            return 'this is a sample module_manager description,' \
-                   ' this sample will tell you how to' \
-                   ' create a module_manager for Figbox'
-
-        @bp.post('/show_body')
-        def show_body(body: str = Body(..., embed=True)):
-            return f'your body is: {body}'
-
-        # テーブル関連
-        # TODO: change table class
-        @bp.post('/create', description='create data to table')
-        def create(dba: DbAdaptor = Depends(DbAdaptor(SampleTable).dba),
-                   data: str = Body(..., embed=True)):
-            """create data into the table"""
-            data = SampleTable(data=data,
-                               link=str(random.randint(0, 99999)),
-                               title=data,
-                               content=data)
-            return dba.add(data)
-
-        # TODO: change table class
-        @bp.get('/read', description='read data from table')
-        def read(dba: DbAdaptor = Depends(DbAdaptor(SampleTable).dba)):
-            """read data from table"""
-            return dba.read_all()
-
-        # TODO: change table class
-        @bp.put('/update', description='update')
-        def update(id: int, dba: DbAdaptor = Depends(DbAdaptor(SampleTable).dba),
-                   data: str = Body(..., embed=True)):
-            sample_data = dba.read_by_id(id)
-            sample_data.data = data
-            return dba.update(sample_data)
-
-        # TODO: change table class
-        @bp.delete('/delete', description='delete a data')
-        def delete(id: int, dba: DbAdaptor = Depends(DbAdaptor(SampleTable).dba)):
-            return dba.delete(id)
-
-        # TODO: change table class
-        @bp.get('/page/{link}', description='show a page')
-        def show_page(link: str, page_adaptor: PageAdaptor = Depends()):
-            return page_adaptor.bind(SampleTable, link, 'sample/temp.html')
-
-        # 任意なプレフィックスを作成する為
-        abc_bp = self._register_free_prefix('/abc', 'abc')
-
-        @abc_bp.get('/sample', description='you used a free prefix')
-        def abc_sample():
-            return 'you used a free prefix'
+class SimpleKey(SecurityModule, TableModule):
 
     def get_table(self) -> list:
-        # TODO: change table class
-        return [SampleTable]
+        return [SimpleKeyTable]
+
+    def get_filters(self, module: Module) -> List[Callable]:
+        def simple_filter(a: str):
+            ...
+
+        return [simple_filter]
 
     def _get_tag(self) -> str:
-        # TODO: change tag
-        return 'サンプルモジュール'
+        return 'セキュリティキー作成ツール'
 
     def get_module_name(self) -> str:
-        # TODO: change module name
-        return 'sample'
+        return 'simple_key'
 
 
-# TODO: change module name
-sample = Sample()
+simple_key = SimpleKey()
--- table.py
+++ table.py
@@ -1,10 +1,9 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, DateTime
 
-from app.core.table_class import PageTable
+from app.core.table_class import DateCreateTable
 
 
 # see https://fastapi.tiangolo.com/tutorial/sql-databases/
-# TODO: change table class name
-class SampleTable(PageTable):
-    __tablename__ = 'sample'
-    data = Column(String(250))
+class SimpleKeyTable(DateCreateTable):
+    __tablename__ = 'simple_key'
+    key = Column(String(48), index=True, unique=True)
