ab506b17e8f7716176653e7e062e68d229605cbc
劉達 <ryu@anchorz.co.jp>
2021-09-29 21:59:02 +0900

    バックアップ

--- __init__.py
+++ __init__.py
@@ -1,17 +1,25 @@
-from typing import List, Callable
+from typing import List, Callable, Optional
 
-from app.core.module_class import Module, TableModule
+from fastapi import APIRouter
+
+from app.core.module_class import Module, TableModule, ApiModule
 from app.core.module_class.SecurityModule import SecurityModule
 from app.modules.simple_key.table import SimpleKeyTable
 
 
-class SimpleKey(SecurityModule, TableModule):
+class SimpleKey(ApiModule, SecurityModule, TableModule):
+    # キーをチェックが必要かどうか、必要な時はデータベースが空きかどうかをチェックし
+    # 空きだと最初の行を入れる
+    is_need_to_check_key = True
+
+    def _register_api_bp(self, bp: APIRouter):
+        pass
 
     def get_table(self) -> list:
         return [SimpleKeyTable]
 
     def get_filters(self, module: Module) -> List[Callable]:
-        def simple_filter(a: str):
+        def simple_filter(key: Optional[str] = None):
             ...
 
         return [simple_filter]
